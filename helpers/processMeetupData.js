ed6f01576bed277c80f22e7c9dae35b877cc7987
Ania <ania.makes@gmail.com>
2018-06-26 16:40:27 +0100

    Can query Contentful. Can query Meetup. Set up initial data manipulation to process data about all subscribed meetups

---
+++ helpers/processMeetupData.js
@@ -0,0 +1,20 @@
+const processMeetupData = arrayOfMeetups => {
+  console.log(arrayOfMeetups);
+  let outputArray = [];
+
+  arrayOfMeetups.forEach(meetup => {
+    let thisMeetup = {};
+    thisMeetup.meetupId = meetup.id;
+    thisMeetup.name = meetup.name;
+    thisMeetup.url = meetup.link;
+    thisMeetup.nextEvent = meetup.next_event ? meetup.next_event.id : 0;
+
+    outputArray.push(thisMeetup);
+  });
+
+  return outputArray;
+};
+
+module.exports = {
+  processMeetupData
+};
--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 require("dotenv").config();
 
-const { createClient } = require("contentful");
+const { createClient } = require("contentful-management");
 const Main = require("apr-main");
 
-const { CONTENTFUL_TOKEN, CONTENTFUL_SPACE } = process.env;
+const { CONTENTFUL_TOKEN, CONTENTFUL_SPACE, MEETUP_KEY } = process.env;
+
+const { processMeetupData } = require("./helpers/processMeetupData");
+
+const meetup = require("meetup-api")({
+  key: MEETUP_KEY
+});
 
 const client = createClient({
-  space: CONTENTFUL_SPACE,
+  // space: CONTENTFUL_SPACE,
   accessToken: CONTENTFUL_TOKEN
 });
 
-Main(async () => {
-  const entries = await client.getEntries();
-  console.log(entries.items);
+meetup.getSelfGroups((err, res) => {
+  // console.log(err, res);
+  console.log(processMeetupData(res));
 });
+
+// Get all Entries
+// Main(async () => {
+//   const space = await client.getSpace(CONTENTFUL_SPACE);
+//   const entries = await space.getEntries();
+//   console.log(entries.items);
+// });
+
+// Main(async () => {
+//   const space = await client.getSpace(CONTENTFUL_SPACE);
+//   const contentTypeList = await space.getContentTypes();
+//   console.log(contentTypeList);
+// });
