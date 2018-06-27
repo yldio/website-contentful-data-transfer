2e47df9f640b7c97d751c2d68bfd6347f29be07a
Ania <ania.makes@gmail.com>
2018-06-27 17:05:50 +0100

    fixed tests: due to refactoring the imports weren't working any more, needed to be updated to new names and structure

--- tests/processMeetupEvent.test.js
+++ tests/processMeetupEvent.test.js
@@ -1,23 +1,23 @@
-const { processMeetupEvent } = require('../helpers/processMeetupEvent');
-const { rawMeetupEventData } = require('../resources/rawMeetupEventData');
+const processMeetupEvent = require("../helpers/process-meetup-event");
+const { rawMeetupEventData } = require("../resources/rawMeetupEventData");
 
-describe('processes Meetup event', () => {
-  it('returns desired structure', () => {
+describe("processes Meetup event", () => {
+  it("returns desired structure", () => {
     const incomingData = rawMeetupEventData;
     const desiredOutcome = {
-      name: 'ReactJS Girls #11 -  Reason, the good, the bad and ugly bits',
+      name: "ReactJS Girls #11 -  Reason, the good, the bad and ugly bits",
       duration: 9000000,
       time: 1531846800000,
-      localTime: '18:00',
-      date: '2018-07-17',
+      localTime: "18:00",
+      date: "2018-07-17",
       venue: {
-        name: 'JP Morgan',
-        address1: '4 John Carpenter St',
+        name: "JP Morgan",
+        address1: "4 John Carpenter St",
         address2: 0,
         address3: 0,
-        city: 'London'
+        city: "London"
       },
-      link: 'https://www.meetup.com/ReactJS-Girls-London/events/251946999/'
+      link: "https://www.meetup.com/ReactJS-Girls-London/events/251946999/"
     };
 
     expect(processMeetupEvent(incomingData)).toMatchObject(desiredOutcome);
